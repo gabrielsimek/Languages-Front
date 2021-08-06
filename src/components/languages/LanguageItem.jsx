16e20e1a78084abd18afcd00816577014f55b13a
Gabriel Simek <gabrielsimek@outlook.com>
2021-08-06 13:23:45 -0700

    Language Item component

---
+++ src/components/languages/LanguageItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const LanguageItem = ({ name, designedBy, firstAppeared, website, logo }) => {
+  return (
+    <figure>
+      <img src={logo} alt={`${name} logo`}/>
+      <figcaption>
+        <h2>{name}</h2>
+        <p>Designed By: {designedBy}</p>
+        <p>First Appeared: {firstAppeared}</p>
+        <p>Website: {website}</p>
+      </figcaption>
+    </figure>
+  );
+};
+
+LanguageItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  designedBy: PropTypes.string.isRequired,
+  firstAppeared: PropTypes.string.isRequired,
+  website: PropTypes.string.isRequired,
+  logo: PropTypes.string.isRequired,
+};
