f90786040f04b1832bee454b00907a0d2b849863
Gabriel Simek <gabrielsimek@outlook.com>
2021-08-06 14:24:49 -0700

    fetchLanguage function

--- src/services/languagesApi.js
+++ src/services/languagesApi.js
@@ -2,10 +2,13 @@ const BASE_URL = 'http://localhost:8001/languages';
 
 const fetchLanguages = async () => {
   const res = await fetch(BASE_URL);
-
   const languages = await res.json();
-  //   console.log(languages);
   return languages;
 };
 
-export { fetchLanguages };
+const fetchLanguage = async (id) => {
+  const res = await fetch(`http://localhost:8001/languages/${id}`);
+  const language = await res.json();
+  return language;
+};
+export { fetchLanguages, fetchLanguage };
