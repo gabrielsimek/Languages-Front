7b94789373fe6e54c71dfe569c7d3be9a3afb4ff
Gabriel Simek <gabrielsimek@outlook.com>
2021-08-07 23:39:44 -0700

    add link to edit

--- src/components/details/LanguageDetails.jsx
+++ src/components/details/LanguageDetails.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useLanguage } from '../../state/languages';
 const LanguageDetails = () => {
   const [language, loading] = useLanguage();
 
   if(loading) return <h1>Loading...</h1>;
   return (
-    <article>
-      <h2>{language.name}</h2>
-      <img src={language.logo} alt={`${language.name} logo`}/>
-      <p>Designed By: {language.designedBy}</p>
-      <p>First Appeared: {language.firstAppeared}</p>
-      <a href={language.website}>Website Link</a>
-    </article>
+    <>
+      <article>
+        <h2>{language.name}</h2>
+        <img src={language.logo} alt={`${language.name} logo`}/>
+        <p>Designed By: {language.designedBy}</p>
+        <p>First Appeared: {language.firstAppeared}</p>
+        <a href={language.website}>Website Link</a>
+      </article>
+      <Link to={`/edit/${language.id}`} >Edit</Link>
+    </>
   );
 };
 
