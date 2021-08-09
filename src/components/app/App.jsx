57edb43d5473a58ca03e92f59fdf267bcf2a41db
Gabriel Simek <gabrielsimek@outlook.com>
2021-08-09 11:36:12 -0700

    change import

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -7,7 +7,7 @@ import LanguagePage from '../languages/LanguagePage';
 import LanguageDetails from '../details/LanguageDetails';
 import LanguageAddPage from '../language-add/LanguageAddPage';
 import LanguageEditPage from '../language-edit/LanguageEditPage';
-import Header from '../header/header';
+import Header from '../header/Header';
 import './App.css';
 export default function App() {
   return (
